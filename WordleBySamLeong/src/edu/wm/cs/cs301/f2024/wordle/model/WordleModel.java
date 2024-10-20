347e7e6100c21f4c1f8aabc032186c4f1719f912
Sam <sleong.wm@edu>
2024-10-20 17:50:38 -0400

    Added a check to see if a word is a viable five letter word

--- WordleBySamLeong/src/edu/wm/cs/cs301/f2024/wordle/model/WordleModel.java
+++ WordleBySamLeong/src/edu/wm/cs/cs301/f2024/wordle/model/WordleModel.java
@@ -35,12 +35,13 @@ public class WordleModel {
 	 * the game grid
 	 */
 	private WordleResponse[][] wordleGrid;
+
 	/*
-	 * constructer that innitilizes game size values 
+	 * constructer that innitilizes game size values
 	 */
 	public WordleModel() {
 		/*
-		 * default game values 
+		 * default game values
 		 */
 		this.currentColumn = -1;
 		this.currentRow = 0;
@@ -64,11 +65,12 @@ public class WordleModel {
 		 */
 		this.statistics = new Statistics();
 	}
-	
+
 	private void createWordList() {
 		ReadWordsRunnable runnable = new ReadWordsRunnable(this);
 		new Thread(runnable).start();
 	}
+
 	/*
 	 * resets the game for next game
 	 */
@@ -85,6 +87,7 @@ public class WordleModel {
 		generateCurrentWord();
 		this.guess = new char[columnCount];
 	}
+
 	/*
 	 * selects a random viable word to be the goal word and updates currentWord
 	 */
@@ -104,7 +107,7 @@ public class WordleModel {
 		int size = wordList.size();
 		return random.nextInt(size);
 	}
-	
+
 	private WordleResponse[][] initializeWordleGrid() {
 		WordleResponse[][] wordleGrid = new WordleResponse[maximumRows][columnCount];
 
@@ -116,13 +119,16 @@ public class WordleModel {
 
 		return wordleGrid;
 	}
+
 	/*
 	 * set method used to update the wordlist for availalbe guesses
+	 * 
 	 * @param a new list of viable words
 	 */
 	public void setWordList(List<String> wordList) {
 		this.wordList = wordList;
 	}
+
 	/*
 	 * chooses a random word from the wordlist and sets currentWord to it
 	 */
@@ -130,8 +136,10 @@ public class WordleModel {
 		int index = getRandomIndex();
 		currentWord = wordList.get(index).toCharArray();
 	}
+
 	/*
 	 * sets the character in the current column and update the grid
+	 * 
 	 * @param inputted character
 	 */
 	public void setCurrentColumn(char c) {
@@ -144,12 +152,11 @@ public class WordleModel {
 		 * adjusts grid and the guess char[]
 		 */
 		guess[currentColumn] = c;
-		wordleGrid[currentRow][currentColumn] = new WordleResponse(c,
-				Color.WHITE, Color.BLACK);
+		wordleGrid[currentRow][currentColumn] = new WordleResponse(c, Color.WHITE, Color.BLACK);
 	}
+
 	/*
 	 * sets the current col to null and walks currentColumn left
-	 * TODO should be MATH.min
 	 */
 	public void backspace() {
 		/*
@@ -158,98 +165,133 @@ public class WordleModel {
 		wordleGrid[currentRow][currentColumn] = null;
 		guess[currentColumn] = ' ';
 		this.currentColumn--;
-		this.currentColumn = Math.max(currentColumn, 0);
+		this.currentColumn = Math.max(currentColumn, -1);
 	}
+
 	/**
 	 * gets the current row in the wordleGrid
+	 * 
 	 * @return returns a row as an array
 	 */
 	public WordleResponse[] getCurrentRow() {
 		return wordleGrid[getCurrentRowNumber()];
 	}
+
 	/*
 	 * get method for currentRow
 	 */
 	public int getCurrentRowNumber() {
 		return currentRow - 1;
 	}
-	
+
 	/*
 	 * sets the background colors of a guessed word and locks in the word
-	 * @return returns false if the current row is the last one
-	 * TODO currentcolumn = -1 is suspicious
+	 * 
+	 * @return returns false if the current row is the last one TODO currentcolumn =
+	 * -1 is suspicious
 	 */
-	public boolean setCurrentRow() {		
-		for (int column = 0; column < guess.length; column++) {
-			Color backgroundColor = AppColors.GRAY;
-			Color foregroundColor = Color.WHITE;
-			if (guess[column] == currentWord[column]) {
-				backgroundColor = AppColors.GREEN;
-			} else if (contains(currentWord, guess, column)) {
-				backgroundColor = AppColors.YELLOW;
+	public boolean setCurrentRow() {
+		if (isWordViable()) {
+			for (int column = 0; column < guess.length; column++) {
+				Color backgroundColor = AppColors.GRAY;
+				Color foregroundColor = Color.WHITE;
+				if (guess[column] == currentWord[column]) {
+					backgroundColor = AppColors.GREEN;
+				} else if (contains(currentWord, guess, column)) {
+					backgroundColor = AppColors.YELLOW;
+				}
+
+				wordleGrid[currentRow][column] = new WordleResponse(guess[column], backgroundColor, foregroundColor);
 			}
-			
-			wordleGrid[currentRow][column] = new WordleResponse(guess[column],
-					backgroundColor, foregroundColor);
-		}
-		
-		currentColumn = -1;
-		currentRow++;
-		guess = new char[columnCount];
-		
+
+			currentColumn = -1;
+			currentRow++;
+			guess = new char[columnCount];
+
+			return currentRow < maximumRows;
+		} 
 		return currentRow < maximumRows;
 	}
-	
+
 	private boolean contains(char[] currentWord, char[] guess, int column) {
 		for (int index = 0; index < currentWord.length; index++) {
 			if (index != column && guess[column] == currentWord[index]) {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
+
 	/*
 	 * get method to access the wordleGrid
+	 * 
 	 * @return returns the grid of characers for the game
 	 */
 	public WordleResponse[][] getWordleGrid() {
 		return wordleGrid;
 	}
+
 	/*
 	 * get method to access the the maxrow size
+	 * 
 	 * @return returns number of rows ingame
 	 */
 	public int getMaximumRows() {
 		return maximumRows;
 	}
+
 	/*
 	 * get method to access the number of columns
+	 * 
 	 * @return returns the columns in the game
 	 */
 	public int getColumnCount() {
 		return columnCount;
 	}
+
 	/*
 	 * get method to access the current column
+	 * 
 	 * @return current column position
 	 */
 	public int getCurrentColumn() {
 		return currentColumn;
 	}
+
 	/*
 	 * get method to access the amount of guessed words
+	 * 
 	 * @return gets the number of guessed words
 	 */
 	public int getTotalWordCount() {
 		return wordList.size();
 	}
+
 	/*
 	 * get method to access the statistics object
+	 * 
 	 * @return returns the statisitcs object
 	 */
 	public Statistics getStatistics() {
 		return statistics;
+	}
+	/*
+	 * checks if the current guess is a viable 5 letter word
+	 */
+	public boolean isWordViable() {
+		String currentInput = "";
+		/*
+		 * grabs guess and makes it into a string
+		 */
+		for (char letter:this.guess) {
+			currentInput += letter;
+		}
+		currentInput = currentInput.toLowerCase();
+		if (this.wordList.contains(currentInput)) {
+			return true;
+		}
+		return false;
 	}
 
 }
