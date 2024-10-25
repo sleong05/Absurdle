b8453ff72b9c17b6f007aa1670d9819949f8bc1a
Sam <sleong.wm@edu>
2024-10-25 18:31:18 -0400

    fixed overloaded switch for "enter" case by moving code to method

--- WordleBySamLeong/src/edu/wm/cs/cs301/f2024/wordle/controller/KeyboardButtonAction.java
+++ WordleBySamLeong/src/edu/wm/cs/cs301/f2024/wordle/controller/KeyboardButtonAction.java
@@ -33,6 +33,66 @@ public class KeyboardButtonAction extends AbstractAction {
 		this.model = model;
 	}
 	
+	private void enterCase() {
+		/**
+		 * checks if the current column is the last column
+		 */
+		if (model.getCurrentColumn() >= (model.getColumnCount() - 1)) {
+			/*
+			 * sets the background for the letters and stores true in more rows if not on last row
+			 */
+			boolean moreRows = model.setCurrentRow(); 
+			/*
+			 * gets the current row and stores it in an array of WordleResponses
+			 */
+			WordleResponse[] currentRow = model.getCurrentRow(); 
+			int greenCount = 0;
+			
+			for (WordleResponse wordleResponse : currentRow) {
+				/*
+				 * sets the background and foreground colors
+				 */
+				view.setColor(Character.toString(wordleResponse.getChar()),
+						wordleResponse.getBackgroundColor(), 
+						wordleResponse.getForegroundColor());
+				/*
+				 * stores how many correct characters were inputed
+				 */
+				if (wordleResponse.getBackgroundColor().equals(AppColors.GREEN)) {
+					greenCount++;
+				} 
+			}
+			/*
+			 * checks if all the characters are green
+			 */
+			if (greenCount >= model.getColumnCount()) {
+				/*
+				 * updates the screen
+				 */
+				view.repaintWordleGridPanel();
+				/*
+				 * brings up the post-game stats and adjusts for the win
+				 */
+				model.getStatistics().incrementTotalGamesPlayed();
+				int currentRowNumber = model.getCurrentRowNumber();
+				model.getStatistics().addWordsGuessed(currentRowNumber);
+				int currentStreak = model.getStatistics().getCurrentStreak();
+				model.getStatistics().setCurrentStreak(++currentStreak);
+				new StatisticsDialog(view, model);
+			} else if (!moreRows) {
+				/*
+				 * brings up post-game stats and adjusts for loss when there are no more rows
+				 */
+				view.repaintWordleGridPanel();
+				model.getStatistics().incrementTotalGamesPlayed();
+				model.getStatistics().setCurrentStreak(0);
+				new StatisticsDialog(view, model);
+			} else {
+				view.repaintWordleGridPanel();
+			}
+		}
+	}
+	
 	/*
 	 * Used whenever and actions is performed to either input a character, delete a character, or enter a five-letter word
 	 * After an enter, it adjusts the view and repaints it to the screen
@@ -52,63 +112,7 @@ public class KeyboardButtonAction extends AbstractAction {
 		switch (text) {
 		
 		case "Enter":
-			/**
-			 * checks if the current column is the last column
-			 */
-			if (model.getCurrentColumn() >= (model.getColumnCount() - 1)) {
-				/*
-				 * sets the background for the letters and stores true in more rows if not on last row
-				 */
-				boolean moreRows = model.setCurrentRow(); 
-				/*
-				 * gets the current row and stores it in an array of WordleResponses
-				 */
-				WordleResponse[] currentRow = model.getCurrentRow(); 
-				int greenCount = 0;
-				
-				for (WordleResponse wordleResponse : currentRow) {
-					/*
-					 * sets the background and foreground colors
-					 */
-					view.setColor(Character.toString(wordleResponse.getChar()),
-							wordleResponse.getBackgroundColor(), 
-							wordleResponse.getForegroundColor());
-					/*
-					 * stores how many correct characters were inputed
-					 */
-					if (wordleResponse.getBackgroundColor().equals(AppColors.GREEN)) {
-						greenCount++;
-					} 
-				}
-				/*
-				 * checks if all the characters are green
-				 */
-				if (greenCount >= model.getColumnCount()) {
-					/*
-					 * updates the screen
-					 */
-					view.repaintWordleGridPanel();
-					/*
-					 * brings up the post-game stats and adjusts for the win
-					 */
-					model.getStatistics().incrementTotalGamesPlayed();
-					int currentRowNumber = model.getCurrentRowNumber();
-					model.getStatistics().addWordsGuessed(currentRowNumber);
-					int currentStreak = model.getStatistics().getCurrentStreak();
-					model.getStatistics().setCurrentStreak(++currentStreak);
-					new StatisticsDialog(view, model);
-				} else if (!moreRows) {
-					/*
-					 * brings up post-game stats and adjusts for loss when there are no more rows
-					 */
-					view.repaintWordleGridPanel();
-					model.getStatistics().incrementTotalGamesPlayed();
-					model.getStatistics().setCurrentStreak(0);
-					new StatisticsDialog(view, model);
-				} else {
-					view.repaintWordleGridPanel();
-				}
-			}
+			enterCase();
 			break;
 		case "Backspace":
 			/*
